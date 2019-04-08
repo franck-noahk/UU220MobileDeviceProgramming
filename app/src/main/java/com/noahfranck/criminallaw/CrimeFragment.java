2a4ec1b78301ad7d86334a025fe58b503b9770ba
Noah <franck.noahk@gmail.com>
2019-04-08 02:21:15 -0500

    Wired up Edit text widget to the text in the fragment.

--- app/src/main/java/com/noahfranck/criminallaw/CrimeFragment.java
+++ app/src/main/java/com/noahfranck/criminallaw/CrimeFragment.java
@@ -2,12 +2,18 @@ package com.noahfranck.criminallaw;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
 	private Crime mCrime;
+	private EditText mTitleField;
+	private Button mDateButton
 
 	@Override
 	public void onCreate(Bundle savedINstanceState){
@@ -18,6 +24,24 @@ public class CrimeFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 		View v = inflater.inflate(R.layout.fragment_crime,container,false);
+		mTitleField = (EditText) v.findViewById(R.id.crime_title);
+		mTitleField.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+				//intentionally left blank
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				mCrime.setTitle(s.toString());
+			}
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				//also left blank
+			}
+		});
+
 		return v;
 	}
 
