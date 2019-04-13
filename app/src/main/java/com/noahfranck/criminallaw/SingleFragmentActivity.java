30853e0625d878541bff069d240d27fb3a5507b6
Noah F <franck.noahk@gmail.com>
2019-04-13 00:42:38 -0500

    Wrote the abstracted version of the class to be pulled out of the simble fragment viewer.

--- app/src/main/java/com/noahfranck/criminallaw/SingleFragmentActivity.java
+++ app/src/main/java/com/noahfranck/criminallaw/SingleFragmentActivity.java
@@ -1,5 +1,8 @@
 package com.noahfranck.criminallaw;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 /*
@@ -9,4 +12,19 @@ fragment.
  */
 
 public abstract class SingleFragmentActivity extends AppCompatActivity {
+	protected abstract Fragment createFragment();
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_fragment);
+
+		FragmentManager fm = getSupportFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+		if(fragment == null){
+			fragment = createFragment();
+			fm.beginTransaction()
+					.add(R.id.fragment_container, fragment)
+					.commit();
+		}
+	}
 }
