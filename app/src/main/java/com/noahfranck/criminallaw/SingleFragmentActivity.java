a4125d7a67515d75bec4f83ba027185c85f1fd0c
Noah F <franck.noahk@gmail.com>
2019-04-13 00:37:16 -0500

    Setting up Abstract class, to abstract away the simpleness of the repeditive java of one activity.

---
+++ app/src/main/java/com/noahfranck/criminallaw/SingleFragmentActivity.java
@@ -0,0 +1,12 @@
+package com.noahfranck.criminallaw;
+
+import android.support.v7.app.AppCompatActivity;
+
+/*
+Goal of the class is to abstract the cod einitally done in CrimeActivity and
+put it in here so that it can be the same used for whenever we just need one
+fragment.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+}
