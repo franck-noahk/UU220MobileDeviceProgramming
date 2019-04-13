d8e442bd2ae14a7871f27fdcc48add146ca10f4d
Noah F <franck.noahk@gmail.com>
2019-04-13 00:06:27 -0500

    Added Singleton of CrimeLab, and plans for it.

---
+++ app/src/main/java/com/noahfranck/criminallaw/CrimeLab.java
@@ -0,0 +1,9 @@
+package com.noahfranck.criminallaw;
+/*
+Singleton class- only one copy exists, it exists as long as the app is stored inside of memory,
+and if it leaves memory, then it is deleted.
+
+CrimeLab goal is private constructor and public get() method
+ */
+public class CrimeLab {
+}
