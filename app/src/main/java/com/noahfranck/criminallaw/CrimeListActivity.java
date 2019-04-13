8154199e5ce9e2f0e35b84244142e6c81ad74ecc
Noah F <franck.noahk@gmail.com>
2019-04-13 01:00:36 -0500

    Actually filling the file with code.

--- app/src/main/java/com/noahfranck/criminallaw/CrimeListActivity.java
+++ app/src/main/java/com/noahfranck/criminallaw/CrimeListActivity.java
@@ -1,4 +1,12 @@
 package com.noahfranck.criminallaw;
 
+import android.support.v4.app.Fragment;
+
 public class CrimeListActivity extends SingleFragmentActivity {
+
+	@Override
+	protected Fragment createFragment(){
+		return new CrimeFragment();
+	}
+
 }
