216baf275bb2fa4c139b146d356b2d4d9a39e95c
Noah F <franck.noahk@gmail.com>
2019-04-13 00:17:00 -0500

    Added the construction of a list of crimes. Added a way to get a crime though method. Added Get Crime UUID

--- app/src/main/java/com/noahfranck/criminallaw/CrimeLab.java
+++ app/src/main/java/com/noahfranck/criminallaw/CrimeLab.java
@@ -2,6 +2,10 @@ package com.noahfranck.criminallaw;
 
 import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 /*
 Singleton class- only one copy exists, it exists as long as the app is stored
 inside of memory,
@@ -10,6 +14,7 @@ and if it leaves memory, then it is deleted.
 CrimeLab goal is private constructor and public get() method
  */
 public class CrimeLab {
+	private List<Crime> mCrimes;
 	private static CrimeLab sCrimeLab;  //use the 's'CrimeLab to show it is
 										// static and not changing
 	//Public way to create a Crime Lab, we create the context file later on
@@ -23,6 +28,18 @@ public class CrimeLab {
 
 	private CrimeLab(Context context){  //private so no other classes can use
 										// this accept for us
+		mCrimes = new ArrayList<>();
+	}
 
+	public List<Crime> getCrimes(){
+		return mCrimes;
+	}
+
+	public Crime getCrime(UUID id){
+		for (Crime crime : mCrimes){
+			if(crime.getID().equals(id)){
+				return crime;
+			}
+		}return null;
 	}
 }
