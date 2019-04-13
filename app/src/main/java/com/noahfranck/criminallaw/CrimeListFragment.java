bdeaee063c2cbfe8b9532d3d5ea3b7e464bdcb99
Noah F <franck.noahk@gmail.com>
2019-04-13 14:40:26 -0500

    Set up the CrimeListFragment to run the view on the screen. Should display black screen.

--- app/src/main/java/com/noahfranck/criminallaw/CrimeListFragment.java
+++ app/src/main/java/com/noahfranck/criminallaw/CrimeListFragment.java
@@ -1,7 +1,26 @@
 package com.noahfranck.criminallaw;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class CrimeListFragment extends Fragment {
-	//currently empty on purpose
+	private RecyclerView mCrimeRecyclerView;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	                         Bundle savedInstanceState){
+		View view = inflater.inflate(R.layout.activity_crime_list,container,
+				false);
+		mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id
+				.crime_recycler_view);
+		mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager
+				(getActivity()));
+
+		return view;
+	}
 }
