f2c2cda880d29ae8581d7057517d6cc6f8bd788b
Noah F <franck.noahk@gmail.com>
2019-04-13 00:48:20 -0500

    Cleaned up CrimeActivity.java.\n Still has old way of doing it in comments and short way is at the EOF

--- app/src/main/java/com/noahfranck/criminallaw/CrimeActivity.java
+++ app/src/main/java/com/noahfranck/criminallaw/CrimeActivity.java
@@ -5,22 +5,24 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CrimeActivity extends AppCompatActivity {
-
+public class CrimeActivity extends SingleFragmentActivity {
+/*  OLD METHOD BEFORE ABSTRACTION
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_fragment);
 
 		FragmentManager fm = getSupportFragmentManager();   //getting a fragment manager
-		/*
+
 		Fragment manager is called because you want to use the suppoort libarray an the
 		appCompactActivity class.
 		To not use the support the library, then you would subclass Activity and call
 		getFragmentManager()
 
 		Fragment Manager maintains a back stack of fragment transactions for navigation.
-		*/
+
 
 
 		Fragment fragment = fm.findFragmentById(R.id.fragment_container);
@@ -32,15 +34,23 @@ public class CrimeActivity extends AppCompatActivity {
 										//beginTransaction method creates and returns an instance
 										// of FragementTransaction. This links them together.
 					.add(R.id.fragment_container, fragment)
-					/*Bread and butter of the transaction, 2 paramiters, container view id and the
-					new CrimeFragment. The view ID is the resource id of the FrameLayout that we
-					defined in activity_fragmentent.xml*/
+					//Bread and butter of the transaction, 2 paramiters,
+					//container view id and the
+					//new CrimeFragment. The view ID is the resource id of the
+					 //FrameLayout that we
+					//defined in activity_fragmentent.xml
 					.commit();
 		}   //The if statement says that We need to create fragment transaction, include one add
 			// operation in it and then commit it
 
 
+*/
 
+
+	//New Way of doing things with abstraction.
+	@Override
+	protected Fragment createFragment(){
+		return new CrimeFragment();
 
 
 	}
